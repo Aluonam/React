9148060ec7c6bce3cf9bdecbe3e72ad726a3ebfa
Paula Cano <paulacanoroman@gmail.com>
2023-05-28 20:29:09 +0200

    Contador Simple +2 o -2
    Uso de UseState para sumar de dos en dos, restar de dos en dos o devolver a 0

--- UseState/contador-simple/app/components/BotonContador.jsx
+++ UseState/contador-simple/app/components/BotonContador.jsx
@@ -4,7 +4,12 @@ import React, { useState } from 'react'
 
 const BotonContador = () => {
 
-const [valorNum, setValorNum] = useState(0)
+    //hook useState para dar valor al numero que se usará al sumar o restar 1
+    const [valorNum, setValorNum] = useState(0)
+
+    //hook useState para dar valor al numero que se usará al sumar o restar 2
+    const [valorNum2, setValorNum2] = useState(0)
+
 
   return (
     <>
@@ -13,6 +18,12 @@ const [valorNum, setValorNum] = useState(0)
     <button onClick={()=>{setValorNum(valorNum+1)}}>+</button>
     <button onClick={()=>{setValorNum(valorNum-1)}}>-</button>
     <button onClick={()=>{setValorNum(0)}}>Retornar a 0</button>
+
+    <span>Boton Contador para incrementar o decrementar de 2 en 2 </span>
+    {valorNum2}
+    <button onClick={()=>{setValorNum2(valorNum2+2)}}>+</button>
+    <button onClick={()=>{setValorNum2(valorNum2-2)}}>-</button>
+    <button onClick={()=>{setValorNum2(0)}}>Retornar a 0</button>
     </>
   )
 }
