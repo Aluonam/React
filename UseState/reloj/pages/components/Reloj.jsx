e75b5ecc44d5fc394d0b69582d4b237aa1330b56
Paula Cano <paulacanoroman@gmail.com>
2023-06-02 10:46:05 +0200

    Reloj
    useState para guardar variable hora
    useEffect para ejecutar continuamente las funciones setInterval(()=>setHora(new Date().toLocaleTimeString()),1000)
    y limpiar cada vez

---
+++ UseState/reloj/pages/components/Reloj.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+const Reloj = () => {
+
+    
+    const [hora, setHora] = useState("No se ha cargado la hora")
+
+    
+    
+    useEffect(() => {
+     const actionPerSecond =  setInterval(()=>setHora(new Date().toLocaleTimeString()),1000)
+     return ()=> clearInterval(actionPerSecond)
+    }, [])
+    
+    
+
+  return (
+    <>
+    <div>Reloj</div>
+    {hora}
+   
+    </>
+  )
+}
+
+export default Reloj
